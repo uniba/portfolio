e787d0c219d23bb11248c757cb31c3b8e7c97bb4
nulltask <nulltask@gmail.com>
2012-11-30 13:43:48 +0900

    add basic auth for admin

--- app.js
+++ app.js
@@ -18,13 +18,20 @@ var app = express()
   , server = module.exports = http.createServer(app);
 
 app.configure(function() {
+  var user = process.env.ADMIN_USERNAME || 'user'
+    , pass = process.env.ADMIN_PASSWORD || 'pass'
+    , basicAuth = express.basicAuth(user, pass);
+
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  
   app.engine('.ejs', cons.ejs);
   app.engine('.html', cons.ejs);
   app.engine('.jade', cons.jade);
+  
   app.enable('trust proxy');
+  
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -35,6 +42,9 @@ app.configure(function() {
     app.use(function(req, res, next) {
       exec('make', next);
     });
+  }
+  if ('production' === app.get('env')) {
+    app.use('/admin', basicAuth);
   }
   
   app.use(app.router);
