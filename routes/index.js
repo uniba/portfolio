e8fff82557a58d58bf4c3246b16036be94dad2e8
nulltask <nulltask@gmail.com>
2012-11-30 13:45:14 +0900

    remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,3 @@
 exports.admin = require('./admin');
 exports.projects = require('./projects');
 exports.tags = require('./tags');
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index.ejs', { title: 'Express' });
-};
