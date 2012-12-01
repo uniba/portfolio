dc23d285543ab717e9c5a7528be01ef6321a625d
nulltask <nulltask@gmail.com>
2012-12-02 03:35:50 +0900

    add content support

---
+++ models/content/plugins/youtube.js
@@ -0,0 +1,32 @@
+
+var qs = require('querystring')
+  , parse = require('url').parse
+  , request = require('request');
+
+module.exports = function(schema, options) {
+  schema.pre('save', function(next) {
+    var self = this
+      , json
+      , videoId
+      , url = parse(this.get('url'));
+    
+    if ('www.youtube.com' !== url.hostname) return next();
+    
+    videoId = qs.parse(url.query).v;
+    json = 'http://gdata.youtube.com/feeds/api/videos/' + videoId + '?alt=json';
+    
+    request({ url: json, json: true }, function(err, res, body) {
+      self
+        .set('type', 'youtube')
+        .set('extend', JSON.stringify(body))
+        .set('title', body.entry.title.$t);
+      
+      request({ url: 'http://i.ytimg.com/vi/' + videoId + '/0.jpg', encoding: 'binary' }, function(err, res, body) {
+        self
+          .set('mime', res.headers['content-type'])
+          .set('image', body);
+        next();
+      });
+    });
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,4 @@
 exports.admin = require('./admin');
 exports.projects = require('./projects');
 exports.tags = require('./tags');
+exports.contents = require('./contents');
