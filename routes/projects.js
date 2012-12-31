61343b1c6ce512c53bb8c66fb089510f01cb3483
nulltask <nulltask@gmail.com>
2012-12-31 17:45:54 +0900

    populate with contents

--- routes/projects.js
+++ routes/projects.js
@@ -5,6 +5,7 @@ var models = require('../models')
 exports.index = function(req, res) {
   Project
     .find()
+    .populate('_contents', { image: 0 })
     .sort('-created')
     .exec(function(err, projects) {
       if (err) return res.send(500, err);
@@ -17,6 +18,7 @@ exports.show = function(req, res) {
   
   Project
     .findOne({ _id: id })
+    .populate('_contents', { image: 0 })
     .exec(function(err, project) {
       if (err) throw err;
       if (!project) return res.send(404);
